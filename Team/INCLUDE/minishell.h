b272966728731102f9243dd1f406f4fd58ebf6d1
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-03-14 13:16:03 +0800

    libft bonus successfully linked in makefile for compile

--- Team/INCLUDE/minishell.h
+++ Team/INCLUDE/minishell.h
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:54:47 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/11 16:38:38 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/03/14 13:10:36 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "libft.h"
 
 #endif
--- Team/SRC/main.c
+++ Team/SRC/main.c
@@ -6,7 +6,7 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:53:45 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/11 16:38:06 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/03/14 13:14:47 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(void)
 {
-	printf("%s\n", readline("test>"));
+	printf("ft_strlen: %d\n", (int)ft_strlen("test>"));
+	printf("ft_toupper from \'a\': %c\n", ft_toupper('a'));
 	return (0);
 }
