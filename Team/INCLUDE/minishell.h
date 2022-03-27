4d1f8e262deecc08a2d38c647513902be4a4ff8d
Yee Jin <yeejin@Yees-MBP.realtek>
2022-03-28 00:21:39 +0800

    trying to include signal, ctrl + C

--- Team/INCLUDE/minishell.h
+++ Team/INCLUDE/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: yang <yang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:54:47 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/14 13:10:36 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/03/27 23:54:00 by yang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,24 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include "libft.h"
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <signal.h>
+# include "../SRC/libft/libft.h"
+
+# define MAXCOM 1000
+
+typedef struct s_prompt
+{
+	char				**envp;
+	struct s_command	*command;
+}	t_prompt;
+
+typedef struct s_command
+{
+	char	**full_cmd;
+	int		infile;
+	int		outfile;
+}	t_command;
 
 #endif
--- Team/SRC/main.c
+++ Team/SRC/main.c
@@ -3,18 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: yang <yang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:53:45 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/14 13:14:47 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/03/28 00:13:59 by yang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../INCLUDE/minishell.h"
 
-int	main(void)
+/* refresh the prompt if user press Ctrl + C 
+ * Problem facing: ^C displayed
+ */
+void new_prompt(int sig)
 {
-	printf("ft_strlen: %d\n", (int)ft_strlen("test>"));
-	printf("ft_toupper from \'a\': %c\n", ft_toupper('a'));
-	return (0);
+	(void)sig;
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+/* get input from user and add it into history list */
+int get_input(char *str)
+{
+	char *buf;
+
+	buf = readline(">>> ");
+	if (strlen(buf) != 0)
+	{
+		add_history(buf);
+		ft_strlcpy(str, buf, ft_strlen(buf) + 1);
+		return (0);
+	}
+	else
+		return (1);
+}
+
+int main(int argc, char *argv[], char *envp[])
+{
+	char input_str[MAXCOM];
+	//t_prompt	*prompt;
+	(void)envp;
+
+	signal(SIGINT, new_prompt);
+	while (argc && argv)
+	{
+		get_input(input_str);
+			//continue;
+		//split_args_pipe(input_str, str);
+		//Perform any shutdown/cleanup
+	}
+	
+	return EXIT_SUCCESS;
 }
