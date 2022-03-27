0dc7b5ba2ae6cca00388e09957cc1511dab6d502
Yee Jin <yeejin@Yees-MBP.realtek>
2022-03-28 01:11:50 +0800

    added envp into struct t_prompt

--- Team/SRC/main.c
+++ Team/SRC/main.c
@@ -6,22 +6,39 @@
 /*   By: yang <yang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:53:45 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/28 00:13:59 by yang             ###   ########.fr       */
+/*   Updated: 2022/03/28 01:11:01 by yang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INCLUDE/minishell.h"
 
-/* refresh the prompt if user press Ctrl + C 
- * Problem facing: ^C displayed
- */
+/* refresh the prompt if user hit Ctrl + C */
 void new_prompt(int sig)
 {
 	(void)sig;
 	printf("\n");
-	rl_replace_line("", 0);
 	rl_on_new_line();
+	rl_replace_line("", 0);
 	rl_redisplay();
+}
+
+/* init environment */
+void init_env(t_prompt *prompt, char *envp[])
+{
+	int	i;
+	int	count;
+
+	i = -1;
+	count = 0;
+	while (envp[++i] != NULL)
+		count++;
+	prompt->envp = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!prompt->envp)
+		return;
+	i = -1;
+	while (envp[++i] != NULL)
+		prompt->envp[i] = ft_strdup(envp[i]);
+	prompt->envp[i] = NULL;
 }
 
 /* get input from user and add it into history list */
@@ -43,9 +60,14 @@ int get_input(char *str)
 int main(int argc, char *argv[], char *envp[])
 {
 	char input_str[MAXCOM];
-	//t_prompt	*prompt;
+	t_prompt	*prompt;
 	(void)envp;
 
+	prompt = malloc(sizeof(t_prompt));
+	if (!prompt)
+		return EXIT_FAILURE;
+	ft_memset(prompt, 0, sizeof(t_prompt));
+	init_env(prompt, envp);
 	signal(SIGINT, new_prompt);
 	while (argc && argv)
 	{
