f6125e09c919a3054b22418089a01774efb10322
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-03-30 19:59:56 +0800

    add builtin commands.

--- Team/SRC/main.c
+++ Team/SRC/main.c
@@ -6,15 +6,16 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 16:53:45 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/03/14 13:14:47 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/03/17 15:46:20 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("ft_strlen: %d\n", (int)ft_strlen("test>"));
-	printf("ft_toupper from \'a\': %c\n", ft_toupper('a'));
-	return (0);
+	char *line = readline("my-rl-example> ");
+	printf("You entered: \"%s\"\n", line);
+	free(line);
+	return 0;
 }
