b3c05b844b1bdc3ce5c278a20d81d2e1c91b7ca9
Yee jin Ang <yeejin0406@gmail.com>
2022-04-25 15:23:41 +0800

    add parser files

---
+++ Team/SRC/parser/expand_utils.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yang <yang@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 11:22:23 by yang              #+#    #+#             */
+/*   Updated: 2022/04/25 11:26:46 by yang             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "yeejin.h"
+
+int	is_name(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isalpha(str[i]) && str[i] != '_')
+		return (0);
+	while (str[++i])
+	{
+		if (!is_env(str[i]))
+			return (0);
+	}
+	return (1);
+}
+
+int	get_env_pos(char *str)
+{
+	int	i;
+	int	quote;
+
+	i = -1;
+	quote = 0;
+	while (str[++i])
+	{
+		if (str[i] == '$')
+			return (i);
+		else if (str[i] == '\'')
+			quote = in_quote(str, i);
+		if (quote > i)
+			i = quote;
+	}
+	return (-1);
+}
+
+char	*get_prefix(char *str, int i)
+{
+	if (i > 0)
+	{
+		return (ft_strndup(str, i));
+	}
+	return (NULL);
+}
+
+char	*get_postfix(char *str, int i)
+{
+	int		postfix_pos;
+
+	postfix_pos = 0;
+	while (str[++i])
+	{
+		if (!is_env(str[i]))
+		{
+			postfix_pos = i;
+			break ;
+		}
+	}
+	if (postfix_pos != 0)
+		return (ft_strndup(str + i, ft_strlen(str) - i));
+	return (NULL);
+}
