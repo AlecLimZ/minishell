6e113ed8de9297c8b5925488255414cf6146362f
Yee Jin <yeejin@Yees-MBP.realtek>
2022-05-09 23:57:43 +0800

    remove test file

--- Team/SRC/libft/ft_strdup.c
+++ Team/SRC/libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yang <yang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 23:16:12 by yang              #+#    #+#             */
-/*   Updated: 2021/05/09 23:32:35 by yang             ###   ########.fr       */
+/*   Updated: 2022/05/09 23:55:26 by yang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ char	*ft_strdup(const char *s1)
 	char	*dest;
 	int		length;
 
-	length = ft_strlen(s1);
-	dest = (char *)malloc(sizeof(char) * length + 1);
+	if (!s1)
+		return (NULL);
+	length = (int)ft_strlen(s1);
+	dest = (char *)malloc(sizeof(char) * (length + 1));
 	if (dest == NULL)
 	{
 		errno = ENOMEM;
